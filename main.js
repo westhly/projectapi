1c8d5c0b5172e427bb2754a64e899c3b0fa8536a
WesthlySarabia <westhly-carrillo@outlook.com>
2023-10-05 11:38:29 -0500

    add otra pag con math random

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 //nombre, imagen, sexo, ubicacion
 import { getProfile } from "./getProfile.js"
 
+document.addEventListener("DOMContentLoaded",() => {
+    const random = getRandomInt(5,6)
+    request(random)
+})
 
-const request = async () => {
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+const request = async (page) => {
     try{
-        const response = await fetch("https://rickandmortyapi.com/api/character")
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page} `)
 
         const resp = await response.json()
         console.log(resp)
+
+        
 
         getProfile(resp.results)
         console.log(getProfile)
